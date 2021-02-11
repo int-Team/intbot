d018fc41948f3a7502bc2161cff2e72a7cd42ea6
sujang958 <enter7377@naver.com>
2021-02-11 19:01:20 +0900

    hotfix

--- commands/ranking.js
+++ commands/ranking.js
@@ -49,21 +49,24 @@ module.exports = {
  */
 const getRank = async (option, client, message) => {
     var rankArr = await client.db.find().sort(xpmoney[option]).limit(5).toArray();
-    var 단위;
+    var 단위, 돈이야뭐야;
     if (option == "돈") {
         단위 = "원";
+        돈이야뭐야 = "money";
     } else {
         단위 = "경험치";
+        돈이야뭐야 = "xp";
     }
     var discordFields = [];
+
 
     for (var i in rankArr) {
         i = Number(i);
         try {
             var userInfo = await client.users.fetch(rankArr[i]._id);
-            discordFields.push({name: `${i+1}. ${userInfo.username}`, value: rankArr[i].money + " " + 단위});
+            discordFields.push({name: `${i+1}. ${userInfo.username}`, value: rankArr[i][돈이야뭐야] + " " + 단위});
         } catch (e) {
-            discordFields.push({name: `${i+1}. Unknown User`, value: rankArr[i].money + " " + 단위});
+            discordFields.push({name: `${i+1}. Unknown User`, value: rankArr[i][돈이야뭐야] + " " + 단위});
         }
     }
 
