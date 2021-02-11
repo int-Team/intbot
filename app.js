060d8ff81cfee0baf6c8b971a1b07aa69afafa93
sujang958 <enter7377@naver.com>
2021-02-11 20:24:13 +0900

    Update ranking.js developers

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const config = require("./config.json");
 const DB_PW = config.dbpw;
 const token = config.token;
 const MongoDB = require('mongodb');
-const DBClient = new MongoDB.MongoClient(`mongodb://127.0.0.1:27017/`, {
+const DBClient = new MongoDB.MongoClient(`mongodb+srv://int:${DB_PW}@cluster0.gk8if.mongodb.net/intbot?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
 client.db = undefined;
 client.dbchannels = undefined;
 DBClient.connect().then(() => {
