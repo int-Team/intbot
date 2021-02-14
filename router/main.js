be617fdd616de38015ebd3f73d2c3818a62c3690
mswgen <54498184+mswgen@users.noreply.github.com>
2021-02-14 19:08:25 +0900

    asdf

--- router/main.js
+++ router/main.js
@@ -17,116 +17,113 @@ let accessToken = {}
 
 
 module.exports =
-/**
- * @param {express.Application} app express app
- * @param {Client} client client
- */
-(app, client) => {
-    app.get('/', (req,res) => {
-        console.log('req');
-        res.render('../views/index.html');
-    });
+    /**
+     * @param {express.Application} app express app
+     * @param {Client} client client
+     */
+    (app, client) => {
+        app.get('/', (req, res) => {
+            res.render('../views/index.html');
+        });
 
-    app.post('/shop/:id', async (req, res, nextf) => {
-        try {
-            if (req.body.id == undefined || isNaN(req.body.id) || req.params.id != req.body.id) {
-                return res.redirect(redirectURI);
-            }
-            const { id, username } = req.body;
-
-            var userDB = await client.db.findOne({_id: `${id}`});
-            var money = userDB.money;
-
-            res.render('shop.ejs', {
-                username: username,
-                money: money,
-                id: id,
-            })
-        } catch (e) {
-            res.send(`<script>alert("이런! 문제가 발생했어요, 아마도 인트봇의 돈 서비스에 가입 하지 않아 생긴일 같아요!");location.href="${redirectURI}";</script>`);
-        }
-    })
-
-    app.post('/buy_process', async (req, res) => {
-        const { id, goods_id } = req.body;
-        const recaptcha_res = req.body['g-recaptcha-response'];
-
-        console.log(req.body);
-
-        if (recaptcha_res == undefined || recaptcha_res == '') {
-            return res.send(`<script>alert("리캡챠 인증을 해주세요!");history.back();</script>`);
-        }
-        
-        fetch("https://www.google.com/recaptcha/api/siteverify", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: `secret=${config.RECAPTCHA_SECRET}&response=${recaptcha_res}`
-        }).then(re_res => {
-            return re_res.json();
-        }).then(async json => {
-            const { success } = json;
-
-            if (success) {
-                let userDB = await client.db.findOne({_id: `${id}`});
-                if (userDB == undefined) {
-                    return res.send(`<script>alert("이런! 문제가 발생했어요, 아마도 인트봇의 돈 서비스에 가입 하지 않아 생긴일 같아요");history.back();</script>`);
-                } else {
-                    let goodsDB = await client.goods.findOne({_id: `${goods_id}`})
-                    const total = userDB.money - goodsDB.price;
-
-                    if (total < 0) {
-                        return res.send(`<script>alert("돈이 부족합니다.");history.back();</script>`);
-                    } else {
-                        await client.db.findOneAndUpdate({_id: `${id}`}, {
-                            $push: {
-                                goods: goods_id,
-                            },
-                            $set: {
-                                money: Number(total)
-                            }
-                        })
-                        res.send(`<script>alert("성공적으로 구입했어요!");location.href="/"</script>`);
-                    }
+        app.post('/shop/:id', async (req, res, nextf) => {
+            try {
+                if (req.body.id == undefined || isNaN(req.body.id) || req.params.id != req.body.id) {
+                    return res.redirect(redirectURI);
                 }
-            } else {
-                return res.send(`<script>alert("올바르지 않은 리캡챠 토큰입니다!");history.back();</script>`);
+                const { id, username } = req.body;
+
+                var userDB = await client.db.findOne({ _id: `${id}` });
+                var money = userDB.money;
+
+                res.render('shop.ejs', {
+                    username: username,
+                    money: money,
+                    id: id,
+                })
+            } catch (e) {
+                res.send(`<script>alert("이런! 문제가 발생했어요, 아마도 인트봇의 돈 서비스에 가입 하지 않아 생긴일 같아요!");location.href="${redirectURI}";</script>`);
             }
         })
-    })
 
-    app.get('/shop/:id', (a, b) => {
-        return b.redirect(redirectURI);
-    })
+        app.post('/buy_process', async (req, res) => {
+            const { id, goods_id } = req.body;
+            const recaptcha_res = req.body['g-recaptcha-response'];
 
-    app.get('/callback', (req, res) => {
-        const { code } = req.query;
-        // 이론
-        // Oauth로 코드 받았으면 디스코드를 통해 액세스 토큰을 발급받아야 됨
-        // 액세스 토큰으로 유저 ID를 가져올수 있음
-        // 액세스 토큰을 사용하지 않고 API에 접근할때에는
-        // 봇 토큰이 필요함, 디스코드 API 접근시 필요한 토큰은 두가지 종류가 있음
-        // 첫번째 berblabla 토큰, 두번째 봇 토큰
+            if (recaptcha_res == undefined || recaptcha_res == '') {
+                return res.send(`<script>alert("리캡챠 인증을 해주세요!");history.back();</script>`);
+            }
 
+            fetch("https://www.google.com/recaptcha/api/siteverify", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: `secret=${config.RECAPTCHA_SECRET}&response=${recaptcha_res}`
+            }).then(re_res => {
+                return re_res.json();
+            }).then(async json => {
+                const { success } = json;
 
-        oauth.tokenRequest({
-            code: code,
-            scope: "identify",
-            grantType: "authorization_code",
-        }).then(discordOauth_res => {
-            accessToken.token = discordOauth_res.access_token;
-            accessToken.token_type = discordOauth_res.token_type;
-            fetch(`${API_ENDPOINT}/users/@me`, {
-                headers: {
-                    Authorization: `${accessToken.token_type} ${accessToken.token}`
+                if (success) {
+                    let userDB = await client.db.findOne({ _id: `${id}` });
+                    if (userDB == undefined) {
+                        return res.send(`<script>alert("이런! 문제가 발생했어요, 아마도 인트봇의 돈 서비스에 가입 하지 않아 생긴일 같아요");history.back();</script>`);
+                    } else {
+                        let goodsDB = await client.goods.findOne({ _id: `${goods_id}` })
+                        const total = userDB.money - goodsDB.price;
+
+                        if (total < 0) {
+                            return res.send(`<script>alert("돈이 부족합니다.");history.back();</script>`);
+                        } else {
+                            await client.db.findOneAndUpdate({ _id: `${id}` }, {
+                                $push: {
+                                    goods: goods_id,
+                                },
+                                $set: {
+                                    money: Number(total)
+                                }
+                            })
+                            res.send(`<script>alert("성공적으로 구입했어요!");location.href="/"</script>`);
+                        }
+                    }
+                } else {
+                    return res.send(`<script>alert("올바르지 않은 리캡챠 토큰입니다!");history.back();</script>`);
                 }
-            }).then(userRes => {
-                return userRes.json();
-            }).then(json => {
-                res.send(`<form action="/shop/${json.id}" method="post">
+            })
+        })
+
+        app.get('/shop/:id', (a, b) => {
+            return b.redirect(redirectURI);
+        })
+
+        app.get('/callback', (req, res) => {
+            const { code } = req.query;
+            // 이론
+            // Oauth로 코드 받았으면 디스코드를 통해 액세스 토큰을 발급받아야 됨
+            // 액세스 토큰으로 유저 ID를 가져올수 있음
+            // 액세스 토큰을 사용하지 않고 API에 접근할때에는
+            // 봇 토큰이 필요함, 디스코드 API 접근시 필요한 토큰은 두가지 종류가 있음
+            // 첫번째 berblabla 토큰, 두번째 봇 토큰
+
+
+            oauth.tokenRequest({
+                code: code,
+                scope: "identify",
+                grantType: "authorization_code",
+            }).then(discordOauth_res => {
+                accessToken.token = discordOauth_res.access_token;
+                accessToken.token_type = discordOauth_res.token_type;
+                fetch(`${API_ENDPOINT}/users/@me`, {
+                    headers: {
+                        Authorization: `${accessToken.token_type} ${accessToken.token}`
+                    }
+                }).then(userRes => {
+                    return userRes.json();
+                }).then(json => {
+                    res.send(`<form action="/shop/${json.id}" method="post">
                 <input type="hidden" name="id" value="${json.id}">
                 <input type="hidden" name="username" value="${json.username}">
             </form><script>document.querySelector("form").submit()</script>`);
+                })
             })
         })
-    })
-}
+    }
