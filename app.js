e3bf79cd25d8519585d898dba97cf9977f6d2d1f
unknown <enter7377@naver.com>
2021-04-22 23:05:56 +0900

    fix shop code

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 // Dependencies
-const Discord = require('discord.js');
-const MusicClient = require('./struct/Client');
-const fs = require('fs');
-const ascii = require('ascii-table');
-const table = new ascii().setHeading('Command', 'Load Status');
-const { DB_PW, token } = require("./config.json");
-const MongoDB = require('mongodb');
+const Discord = require("discord.js");
+const MusicClient = require("./struct/Client");
+const fs = require("fs");
+const ascii = require("ascii-table");
+const table = new ascii().setHeading("Command", "Load Status");
+const { DB_PW, token, prefix } = require("./config.json");
+const MongoDB = require("mongodb");
 
 
 // Variables 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 5001;
-const prefix = "인트야";
 
 // Discord bot client
 const client = new MusicClient();
-client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
 
 // Database (mongodb+srv://int:${DB_PW}@cluster0.gk8if.mongodb.net/intbot?retryWrites=true&w=majority)
 client.db = undefined;
 client.dbchannels = undefined;
-const DBClient = new MongoDB.MongoClient(`mongodb+srv://int:${DB_PW}@cluster0.gk8if.mongodb.net/intbot?retryWrites=true&w=majority`, {
+const DBClient = new MongoDB.MongoClient(`mongodb://inttest:intbabo1@cluster0-shard-00-00.pd6jy.mongodb.net:27017,cluster0-shard-00-01.pd6jy.mongodb.net:27017,cluster0-shard-00-02.pd6jy.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-1ywkc2-shard-0&authSource=admin&retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 DBClient.connect().then(() => {
-    client.db = DBClient.db('intbot').collection('main');
-    client.goods = DBClient.db('intbot').collection('goods');
-    client.dbchannels = DBClient.db('intbot').collection('channels');
+    client.db = DBClient.db("intbot").collection("main");
+    client.goods = DBClient.db("intbot").collection("goods");
+    client.dbchannels = DBClient.db("intbot").collection("channels");
 });
 
 // Web
-const express = require('express');
-const logger = require('morgan');
-const session = require('express-session');
+const express = require("express");
+const logger = require("morgan");
+const session = require("express-session");
 const app = express();
 
 // Web: Middlewares
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-app.use(express.static('public'));
-app.use(logger('dev'));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static("public"));
+app.use(logger("dev"));
 app.use(express.urlencoded({extended: false}));
 app.use(session({
     secret: "asdfasfmklam3mkm;'MKGH:@90-t",
@@ -51,25 +49,25 @@ app.use(session({
     saveUninitialized: true,
 }));
 
-require('./router/main')(app, client);
+require("./router/main")(app, client);
 
 app.listen(PORT, () => {
     console.log(`Server on : ${PORT}`);
 })
 
 // Discord bot setting
-fs.readdir('./commands/', (err, list) => {
+fs.readdir("./commands/", (err, list) => {
     for (let file of list) {
         try {
             let pull = require(`./commands/${file}`);
             if (pull.name && pull.run && pull.aliases) {
-                table.addRow(file, '✅');
+                table.addRow(file, "✅");
                 for (let alias of pull.aliases) {
                     client.aliases.set(alias, pull.name);
                 }
                 client.commands.set(pull.name, pull);
             } else {
-                table.addRow(file, '❌ -> Error');
+                table.addRow(file, "❌ -> Error");
                 continue;
             }
         } catch (e) { 
@@ -80,70 +78,70 @@ fs.readdir('./commands/', (err, list) => {
     console.log(table.toString());
 });
 
-client.on('ready', () => {
+client.on("ready", () => {
     console.log(`Logged on ${client.user.username}\n-----------------------`);
     setInterval(() => {
         switch (Math.floor(Math.random() * 6)) {
             case 0:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
                         name: `${client.guilds.cache.size}개의 서버에서 활동중!`,
-                        type: 'PLAYING'
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 1:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `이 말은 10초마다 바뀐다는 사실을 아셨나요?`,
-                        type: 'PLAYING'
+                        name: "이 말은 10초마다 바뀐다는 사실을 아셨나요?",
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 2:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `접두사는 인트야 입니다!`,
-                        type: 'PLAYING'
+                        name: "접두사는 인트야 입니다!",
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 3:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `이 봇은 사실 아직 베타 버전이랍니다!`,
-                        type: 'PLAYING'
+                        name: "이 봇은 사실 아직 베타 버전이랍니다!",
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 4:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `인트봇의 문의/신고는 인트야 문의 [내용]!`,
-                        type: 'PLAYING'
+                        name: "인트봇의 문의/신고는 인트야 문의 [내용]!",
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 5:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `인트야 초대로 인트봇을 자신의 서버에 초대해보세요!`,
-                        type: 'PLAYING'
+                        name: "인트야 초대로 인트봇을 자신의 서버에 초대해보세요!",
+                        type: "PLAYING"
                     }
                 });
                 break;
             case 6:
                 client.user.setPresence({
-                    status: 'online',
+                    status: "online",
                     activity: {
-                        name: `인트야 도움`,
-                        type: 'PLAYING'
+                        name: "인트야 도움",
+                        type: "PLAYING"
                     }
                 });
                 break;
@@ -168,8 +166,7 @@ client.on('ready', () => {
     */
 });
 
-client.on('message', async message => {
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+client.on("message", async message => {
     if (message.author.bot) return;
 
     let user = await client.db.findOne({_id: message.author.id});
@@ -197,10 +194,10 @@ client.on('message', async message => {
     }
 });
 
-client.on('message', async message => {
+client.on("message", async message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
-    let args = message.content.substr(prefix.length).trim().split(' ');
+    let args = message.content.substr(prefix.length).trim().split(" ");
     if (client.commands.get(args[0])) {
         client.commands.get(args[0]).run(client, message, args);
     } else if (client.aliases.get(args[0])) {
@@ -219,8 +216,8 @@ client.on('message', async message => {
         for (let curr of valids) {
             let cnt = 0;
             let i = 0;
-            for (let curlet of curr.split('')) {
-                if (curlet[i] && typed.split('')[i] && curlet[i] == typed.split('')[i]) {
+            for (let curlet of curr.split("")) {
+                if (curlet[i] && typed.split("")[i] && curlet[i] == typed.split("")[i]) {
                     cnt++;
                 }
                 i++;
--- router/main.js
+++ router/main.js
@@ -1,177 +1,162 @@
-const express = require("express");
-const { Client } = require("discord.js");
-const config = require("../config.json");
-const redirectURI =
-  "https://discord.com/api/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Fchul0721.iptime.org%3A5001%2Fcallback&response_type=code&scope=identify";
-const CLIENT_SECRET = config.CLIENT_SECRET;
-const API_ENDPOINT = "https://discord.com/api/v8";
-const CLIENT_ID = "798709769929621506";
-const REDIRECT_URI = 'http://localhost:5001/callback';
-// const REDIRECT_URI = "http://asdf/callback";
-const REDIRECT_URI_PROFILE = "http://localhost:5001/profile_callback";
+require('dotenv').config();
 const DiscordOauth2 = require("discord-oauth2");
-const { default: fetch } = require("node-fetch");
-let accessToken = {};
+const Oauth = new DiscordOauth2({
+    version: 'v8',
+    clientId: '798709769929621506',
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI
+});
+// 세션챗 와바
 
 module.exports =
   /**
-   * @param {express.Application} app express app
-   * @param {Client} client client
+   * @param {import('express').Application} app express app
+   * @param {import('discord.js').Client} client client
    */
-  (app, client) => {
+(app, client) => {
     app.get("/", (req, res) => {
-      console.log("req");
-      res.render("../views/index.html");
+        res.render("index");
     });
 
-    app.get("/shop/:code", async (req, res, next) => {
-      try {
-        const { code } = req.params;
-
-        if (!code)  res.send(`<script>alert('이런! 뭔가 잘못된것같아요, 다시 로그인해 보아요!');location.href='https://discord.com/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fcallback&response_type=code&scope=identify';</script>`)
-
-        var oauth = new DiscordOauth2({
-          clientId: CLIENT_ID,
-          clientSecret: CLIENT_SECRET,
-          redirectUri: REDIRECT_URI,
-        });
-
-        const token = await oauth
-        .tokenRequest({
-          code: code,
-          scope: "identify",
-          grantType: "authorization_code",
-        });
-        
-        let userJSON = await fetch(`${API_ENDPOINT}/users/@me`, {
-          headers: {
-            Authorization: `${token.token_type} ${token.access_token}`,
-          }
-        });
-        userJSON = await userJSON.json();
-        let userDB = await client.db.findOne({_id: userJSON.id});
-
-        client.goods.insertOne({
-          _id: `${code}`,
-          refresh_token: `${token.refresh_token}`,
-          type: `${token.token_type}`
-        });
-
-        res.render('shop', {
-          code: `${code}`,
-          user: {
-            name: `${userJSON.username}#${userJSON.discriminator}`,
-            money: `${userDB.money}`
-          }
-        });
-      } catch (e) {
-        console.log(e);
-        res.send(`<script>alert('이런! 뭔가 잘못된것같아요, 다시 로그인해 보아요!');location.href='https://discord.com/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fcallback&response_type=code&scope=identify';</script>`)
-      }
-    });
-
-    app.get("/buy_process", async (req, res) => {
-      const { code, itemID } = req.query;
-      console.log(req.query);
-
-      if (!code || !itemID) return res.redirect(`https://discord.com/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fcallback&response_type=code&scope=identify`);
-      let goodsDB = await client.goods.findOne({_id: `${itemID}`});
-      if (!goodsDB) return res.send(`<script>alert('유효하지않은 상품입니다!');history.back();</script>`);
-      let tokenDB = await client.goods.findOne({_id: `${code}`});
-      if (!tokenDB) return res.redirect(`https://discord.com/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fcallback&response_type=code&scope=identify`);
-
-      try {
-        var oauth = new DiscordOauth2({
-          clientId: CLIENT_ID,
-          clientSecret: `${CLIENT_SECRET}`,
-          redirectUri: REDIRECT_URI,
-        });
-
-        let token = await oauth
-        .tokenRequest({
-          refreshToken: `${tokenDB.refresh_token}`,
-          scope: "identify",
-          grantType: `refresh_token`,
-        });
-
-        let userJSON = await fetch(`${API_ENDPOINT}/users/@me`, {
-          headers: {
-            Authorization: `${token.token_type} ${token.access_token}`,
-          }
-        });
-        userJSON = await userJSON.json();
-        console.log(userJSON);
-        let userDB = await client.db.findOne({_id: userJSON.id});
-        
-        if (!userDB) return res.send(`<script>alert('뭔가 잘못된것 같아요, 아마도 인트의 돈 서비스의 가입하지 않아 생긴 일 같아요');history.back();</script>`);
-
-        let total = userDB.money - goodsDB.price;
-
-        if (total < 0) return res.send(`<script>alert('돈이 부족합니다!');history.back();</script>`);
-
-        client.db.findOneAndUpdate({_id: userJSON.id}, {
-          $push: {
-            goods: itemID,
-          }
-        });
-
-        client.goods.deleteOne({_id: code});
-
-        return res.send(`<script>alert('성공적으로 구입했습니다!');location.href="/profile/${userJSON.id}";</script>`);
-      } catch (e) {
-        console.log(e);
-        res.send(`<script>alert('이런! 뭔가 잘못된것같아요, 다시 로그인해 보아요!');location.href='https://discord.com/oauth2/authorize?client_id=798709769929621506&redirect_uri=http%3A%2F%2Flocalhost%3A5001%2Fcallback&response_type=code&scope=identify';</script>`);
-      }
-    });
-
-    app.get("/callback", (req, res) => {
-      return res.redirect(`/shop/${req.query.code}`);
-    });
-
-    
-
-    app.get("/profile/:id", async (req, res) => {
-      const { id } = req.params;
-      var userDB = await client.db.findOne({ _id: id });
-
-      if (userDB == undefined || userDB == null) {
-        res.status(404).send(`<h1>이런! 찾으시려는 유저가 없는것 같아요..</h1>`);
-      } else {
+    app.get('/callback', async (req, res) => {
         try {
-            var discordUser = await client.users.fetch(id);
-            var rank = await getMyRank(discordUser.id, client);
+            const { code } = req.query;
 
-            res.render("profile.ejs", {
-                profile_img: discordUser.displayAvatarURL(),
-                username: `${discordUser.username}`,
-                tag: `${discordUser.tag}`,
-                money: rank.money.count,
-                level: rank.level.count,
-                xp: rank.xp.count,
-                money_rank: rank.money.rank,
-                level_rank: rank.level.rank,
-                xp_rank: rank.xp.rank,
-                goods: userDB.goods,
+            if (!code)
+                return res.redirect(process.env.OAUTH_URL);
+            if (req.session.user_id)
+                return res.redirect('/');
+
+            const token = await Oauth.tokenRequest({
+                code,
+                scope: ['identify'],
+                grantType: 'authorization_code',
+            });
+            const user = await Oauth.getUser(token.access_token);
+            
+            if (!client.users.cache.has(user.id))
+                return res.send(`<script>alert("인트봇이 접근할 수 있는 유저가 아니에요!");location.back();</script>`);
+            
+            const dscUser = await client.users.cache.get(user.id);
+            const userDB = await client.db.findOne({_id: dscUser.id});
+
+            if (!userDB)
+                return res.send(`<script>alert("인트봇에 가입한 유저가 아니에요!");location.back();</script>`);
+
+            req.session.user_id = dscUser.id;
+
+            res.redirect(`/shop`);
+        } catch (e) {
+            console.log(e);
+            res.redirect(process.env.OAUTH_URL);
+        }
+    });
+
+    app.get("/shop", async (req, res) => {
+        try {
+            if (!req.session.user_id)
+                return res.redirect(process.env.OAUTH_URL);
+            
+            if (!client.users.cache.has(req.session.user_id))
+                return res.send(`<script>alert("인트봇이 접근할 수 있는 유저가 아니에요!");location.back();</script>`);
+            
+            const dscUser = await client.users.cache.get(req.session.user_id);
+            const userDB = await client.db.findOne({_id: dscUser.id});
+
+            if (!userDB)
+                return res.send(`<script>alert("인트봇에 가입한 유저가 아니에요!");location.back();</script>`);
+
+            res.render('shop', {
+                user: {
+                    tag: dscUser.tag,
+                    money: userDB.money
+                }
             });
         } catch (e) {
             console.log(e);
-
-            res.render("profile.ejs", {
-                profile_img: `https://blog.kakaocdn.net/dn/cyOIpg/btqx7JTDRTq/1fs7MnKMK7nSbrM9QTIbE1/img.jpg`,
-                username: "Unknown User",
-                tag: "Unknown Tag",
-                money: `unknown`,
-                level: `unknown`,
-                xp: `unknown`,
-                money_rank: `unknown`,
-                level_rank: `unknown`,
-                xp_rank: `unknown`,
-                goods: []
-            });
+            res.redirect(process.env.OAUTH_URL);
         }
-      }
     });
-};
+
+    app.get("/buy/:merch_id", async (req, res) => {
+        const { merch_id } = req.params;
+
+        if (!merch_id)
+            return res.send(`<script>alert("살 수 있는 상품이 아니에요!");location.back();</script>`);
+        if (!req.session.user_id)
+            return res.redirect('/shop');
+        
+        if (!client.users.cache.has(req.session.user_id))
+            return res.send(`<script>alert("인트봇이 접근할 수 있는 유저가 아니에요!");location.back();</script>`);
+        
+        const dscUser = await client.users.cache.get(req.session.user_id);
+        const userDB = await client.db.findOne({_id: dscUser.id});
+        const merch = await client.goods.findOne({_id: merch_id});
+
+        if (!userDB)
+            return res.send(`<script>alert("인트봇 서비스에 가입한 유저가 아니에요!");location.back();</script>`);
+        if (userDB.money < merch.price)
+            return res.send(`<script>alert("인트봇 머니가 부족해요!");location.back();</script>`);
+
+        await client.db.updateOne({_id: req.session.user_id}, {
+            $set: {
+                money: (userDB.money - merch.price),
+            },
+            $push: {
+                goods: merch._id
+            }
+        });
+            
+        res.send(`<script>alert("인트봇 아이템이 구매되었어요!");location.href="/shop";</script>`)
+    });
+
+    app.get('/logout', (req, res) => {
+        req.session.destroy();
+        res.redirect('/');
+    });
+
+      app.get("/profile/:id", async (req, res) => {
+          const { id } = req.params;
+          var userDB = await client.db.findOne({ _id: id });
+
+          if (userDB == undefined || userDB == null) {
+              res.status(404).send("<h1>이런! 찾으시려는 유저가 없는것 같아요..</h1>");
+          } else {
+              try {
+                  var discordUser = await client.users.fetch(id);
+                  var rank = await getMyRank(discordUser.id, client);
+
+                  res.render("profile.ejs", {
+                      profile_img: discordUser.displayAvatarURL(),
+                      username: `${discordUser.username}`,
+                      tag: `${discordUser.tag}`,
+                      money: rank.money.count,
+                      level: rank.level.count,
+                      xp: rank.xp.count,
+                      money_rank: rank.money.rank,
+                      level_rank: rank.level.rank,
+                      xp_rank: rank.xp.rank,
+                      goods: userDB.goods,
+                  });
+              } catch (e) {
+                  console.log(e);
+
+                  res.render("profile.ejs", {
+                      profile_img: "https://blog.kakaocdn.net/dn/cyOIpg/btqx7JTDRTq/1fs7MnKMK7nSbrM9QTIbE1/img.jpg",
+                      username: "Unknown User",
+                      tag: "Unknown Tag",
+                      money: "unknown",
+                      level: "unknown",
+                      xp: "unknown",
+                      money_rank: "unknown",
+                      level_rank: "unknown",
+                      xp_rank: "unknown",
+                      goods: []
+                  });
+              }
+          }
+      });
+  };
 
 
 const getMyRank = async (id, client) => {
@@ -183,13 +168,13 @@ const getMyRank = async (id, client) => {
     rank.xp = {};
     rank.money.rank = moneyRankArr.findIndex(e => {
         return e._id == id;
-    })
+    });
     rank.level.rank = levelRankArr.findIndex(e => {
         return e._id == id;
-    })
+    });
     rank.xp.rank = levelRankArr.findIndex(e => {
         return e._id == id;
-    })
+    });
     rank.level.rank += 1;
     rank.money.rank += 1;
     rank.xp.rank += 1;
@@ -198,4 +183,4 @@ const getMyRank = async (id, client) => {
     rank.xp.count = await (await client.db.findOne({_id: id})).xp;
 
     return rank;
-}
+};
