0a4b8f7273326f1c652b86d12fdd067004a97901
누군가 <59335584+sujang958@users.noreply.github.com>
2021-05-20 01:25:09 +0900

    Fix: router position

--- src/modules/WebServer.js
+++ src/modules/WebServer.js
@@ -4,15 +4,15 @@ const session = require('express-session')
 const app = express()
 
 module.exports = async (client, PORT) => {
-  // Web: Middlewares
   app.set('views', process.cwd() +'/src/views')
   app.set('view engine', 'ejs')
   app.engine('html', require('ejs').renderFile)
   app.use(express.static('public'))
   app.use(logger('dev'))
+  app.use(express.json())
   app.use(express.urlencoded({extended: false}))
   app.use(session({secret: process.env.SESSION_SECRET,resave: false,saveUninitialized: true,}))
 
-  app.listen(PORT, () => console.log(client.color('magenta', '[Web Server]'), `Server on : ${PORT}`))
   require('../router/main')(app, client)
+  app.listen(PORT, () => console.log(client.color('magenta', '[Web Server]'), `Server on : ${PORT}`))
 }
